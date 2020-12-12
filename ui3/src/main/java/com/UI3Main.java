71e03e6a55a5e30d6e15ab87a12c1bef7c813df6
h_dinari <h_dinari@modernisc.com>
2020-12-12 10:33:21 +0330

    #Second-Commit

--- ui3/src/main/java/com/UI3Main.java
+++ ui3/src/main/java/com/UI3Main.java
@@ -3,5 +3,6 @@ package com;
 public class UI3Main {
     public static void main(String[] args) {
         System.out.println("UI3Main ...");
+        System.out.println("TestComment....");
     }
 }
